ab41674badbc7b42354b50ae91c2441c4b546fb8
YounessFTN <youness.fatine1@gmail.com>
2025-02-27 12:08:54 +0100

    remove migartion

--- prisma/migrations/20250213113820_add_image_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Anime" ADD COLUMN     "image" TEXT;
-
--- AlterTable
-ALTER TABLE "Blog" ADD COLUMN     "image" TEXT;
-
--- AlterTable
-ALTER TABLE "Manga" ADD COLUMN     "image" TEXT;
